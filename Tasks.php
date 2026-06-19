c86f716de1f63ce8afabc012d0bebe5ea9aff7df
Dicksonsilayo <dicksonsilayo515@gmail.com>
2026-06-19 16:52:08 +0300

    this is task tracker challenge in Command line interface

---
+++ Tasks.php
@@ -0,0 +1,116 @@
+<?php
+
+class TaskTracker{
+    private $file = "tasks.json";
+public function __construct()
+{
+  if(!file_exists($this->file)){
+    file_put_contents($this->file,json_encode([],JSON_PRETTY_PRINT));
+
+
+  } 
+
+}
+private function readTasks(){
+    $data=file_get_contents($this->file);
+    return json_decode($data,true);
+}
+private function saveTasks($tasks){
+    file_put_contents($this->file,json_encode($tasks,JSON_PRETTY_PRINT));
+}
+
+
+private function getNextId($tasks){
+    if(empty($tasks))
+    {
+        return 1;
+
+    }
+        $ids = array_column($tasks, 'id');
+
+    if (empty($ids)) {
+        return 1;
+    }
+
+    return max(array_column($tasks,'id'))+1;
+
+
+}
+
+
+
+public function add($description){
+    $tasks=$this->readTasks();
+
+    $task=[
+    "id" => $this->getNextId($tasks),
+    "description"=> $description,
+    "status"=>"to do",
+    "created_at"=>date("y-m-d H:i:s"),
+    "updated_at"=>date("y-m-d H:i:s")
+
+    ];
+    $tasks[]=$task;
+    $this->saveTasks($tasks);
+  echo "Task added successfully (ID: {$task['id']})\n";
+
+
+ }
+public function update($id,$description){
+    $tasks=$this->readTasks();
+    foreach($tasks as &$task){
+        if($task['id']==$id){
+            $task['description']=$description;
+            $task['udated_at']=date("Y-m-d H:i:s");
+            $this->saveTasks($tasks);
+              echo "Task updated successfully (ID: {$task['id']})\n";
+              return;
+        }
+    }
+   
+
+}
+ public function delete($id){
+       $tasks=$this->readTasks();
+       foreach($tasks as $task){
+        if($task['id']==$id){
+            unset($tasks[$key]);
+            $this->saveTasks(array_values($tasks));
+            echo "task with id {$id} has been deleted successfully\n\n";
+            return;
+        }
+       }
+    }
+  
+
+
+}
+  $tracker= new TaskTracker();
+  $command= $argv[1] ?? null;
+  switch("$command"){
+    case "add":
+              if (!isset($argv[2])) {
+            echo "Usage: php task.php add \"Task Description\"\n";
+            exit;
+        }
+        $tracker->add($argv[2]);
+        break;
+         case "update":
+        if (!isset($argv[2], $argv[3])) {
+            echo "Usage: php task.php update ID \"New Description\"\n";
+            exit;
+        }
+        $tracker->update($argv[2], $argv[3]);
+        break;
+
+    case "delete":
+        if (!isset($argv[2])) {
+            echo "Usage: php task.php delete ID\n";
+            exit;
+        }
+        $tracker->delete($argv[2]);
+        break;
+    default:
+        echo "\nTask Tracker Commands:\n\n";
+  }
+?>
